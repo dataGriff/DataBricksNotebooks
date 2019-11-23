b95bbdaa131157830f8cab923c5c73ebe39329b8
griff182uk <griff182uk@yahoo.co.uk>
2019-11-23 16:54:15 +0000

    Update: README.md notebooks

---
+++ notebooks/04-Reading-Writing-Data/Python/Includes/Test-Library.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %python
+# MAGIC def dbTest(id, expected, result):
+# MAGIC   assert str(expected) == str(result), "{} does not equal expected {}".format(result, expected)
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC def dbTest[T](id: String, expected: T, result: => T, message: String = ""): Unit = {
+# MAGIC   assert(result == expected, message)
+# MAGIC }
+# MAGIC displayHTML("Imported Test Library...") // suppress output
