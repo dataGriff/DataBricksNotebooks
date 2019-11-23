4c6a04b6c3086e9523a1a4673855e0349749e18c
griff182uk <griff182uk@yahoo.co.uk>
2019-11-23 16:53:13 +0000

    Update: README.md notebooks

---
+++ notebooks/04-Reading-Writing-Data/Python/Includes/Classroom-Setup.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %run "./Dataset-Mounts"
+
+# COMMAND ----------
+
+# MAGIC %run "./Test-Library"
+
+# COMMAND ----------
+
+# MAGIC %run "./Table-Creator"
+
+# COMMAND ----------
+
+displayHTML("All done!")
